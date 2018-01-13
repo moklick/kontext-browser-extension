a711073d06b3277c23c7fc729abff46f927d0e07
arnese <arne.semsrott@okfn.de>
2018-01-13 18:39:49 +0100

    add more afd

--- data.js
+++ data.js
@@ -2,19 +2,19 @@ var data = [
   {
     'forename': 'Björn',
     'name': 'Höcke',
-    'text': ['der in Nazizeitschriften publiziert hat'],
+    'text': ['der gute Kontakte zur NPD hat'],
     'src': ['https://de.wikipedia.org/wiki/Bj%C3%B6rn_H%C3%B6cke#Vermutete_Autorschaft_in_Zeitschriften_des_NPD-Umfelds']
   },
   {
     'forename': 'Alexander',
     'name': 'Gauland',
-    'text': ['der einen Menschen entsorgen lassen will'],
+    'text': ['der Menschen entsorgen lassen will'],
     'src': ['http://www.faz.net/aktuell/politik/bundestagswahl/afd-alexander-gauland-traeumt-von-entsorgung-aydan-oezoguz-15171141.html']
   },
   {
     'forename': 'Alice',
     'name': 'Weidel',
-    'text': ['die die Reichsbürgerthese vertritt, Deutschland sei nicht souverän'],
+    'text': ['die Deutschland als \"nicht souverän\" bezeichnet'],
     'src': ['http://www.zeit.de/politik/deutschland/2017-09/afd-spitzenkandidatin-alice-weidel-email-rechtsradikal']
   },
   {
@@ -32,7 +32,7 @@ var data = [
   {
     'forename': 'Frauke',
     'name': 'Petry',
-    'text': ['die der französischen Rechtsextremen Marine Le Pen gratuliert hat'],
+    'text': ['die Fan der französischen Rechtsextremistin Marine Le Pen ist'],
     'src': ['https://de.wikipedia.org/wiki/Frauke_Petry#Migrationspolitik,_Asylrecht,_Kriminalit%C3%A4tsbek%C3%A4mpfung_und_EU']
   },
   {
@@ -86,13 +86,13 @@ var data = [
   {
     'forename': 'Wilhelm',
     'name': 'von Gottberg',
-    'text': ['der den Holocaus als \"Mittel zur Kriminalisierung der Deutschen\" bezeichnet'],
+    'text': ['der den Holocaust als \"Mittel zur Kriminalisierung der Deutschen\" bezeichnet'],
     'src': ['http://www.zeit.de/politik/deutschland/2017-09/afd-kandidaten-bundestagswahl-abgeordnete']
   },
   {
     'forename': 'Stefan',
     'name': 'Keuter',
-    'text': ['der auf Pegida-Demos \"Lügenpresse\" skandiert hat'],
+    'text': ['der auf Pegida-Demos \"Lügenpresse\" mitskandiert'],
     'src': ['http://www.tagesspiegel.de/politik/neue-abgeordnete-das-sind-die-radikalen-in-der-afd-fraktion/20361302.html']
   },
   {
@@ -106,5 +106,36 @@ var data = [
     'name': 'Bystron',
     'text': ['der laut Verfassungsschutz verfassungsfeindliche Bestrebungen zeigt'],
     'src': ['http://www.tagesspiegel.de/politik/neue-abgeordnete-das-sind-die-radikalen-in-der-afd-fraktion/20361302.html']
+  },
+  {
+    'forename': 'Beatrix',
+    'name': 'von Storch',
+    'text': ['die an der Grenze auch auf Kinder schießen lassen würde'],
+    'src': ['http://www.spiegel.de/politik/deutschland/afd-beatrix-von-storch-wird-im-netz-verspottet-a-1076209.html']
+  },
+  {
+    'forename': 'Frank',
+    'name': 'Pasemann',
+    'text': ['der Verbindungen zur rassistischen Identitäten Bewegungen hat'],
+    'src': ['http://www.zeit.de/politik/deutschland/2017-09/afd-kandidaten-bundestagswahl-abgeordnete']
+  },
+  {
+    'forename': 'Andreas',
+    'name': 'Kalbitz',
+    'text': ['der Vorsitzender des rechtsextremen Vereins Kultur- und Zeitgeschichte, Archiv der Zeit war'],
+    'src': ['http://www.maz-online.de/Brandenburg/Der-Brandenburger-AfD-Landtagsabgeordnete-Andreas-Kalbitz-geraet-wegen-einer-Mitgliedschaft-in-einem-rechten-Verein-unter-Druck']
+  },
+  {
+    'forename': 'Doris',
+    'name': '',
+    'text': ['die im Vorstand eines Reichsbürger-Vereins war'],
+    'src': ['http://haz.de/Nachrichten/Politik/Deutschland-Welt/AfD-Fuerstin-hat-Vergangenheit-in-Reichsbuerger-Szene']
+  },
+  {
+    'forename': 'André',
+    'name': 'Poggenburg',
+    'text': ['der Veranstaltungen mit Neonazis durchführt'],
+    'src': ['http://www.zeit.de/2016/05/afd-andre-poggenburg-sachsen-anhalt-asylpolitik-wahlkampf']
   }
+
 ];
