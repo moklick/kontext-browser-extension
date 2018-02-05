a9dbeda5528dbf2bb9757ea1a369cdd743428b4a
AndyTheGiant <andy.bialystock@gmail.com>
2018-02-05 20:45:34 +0100

    refactor/rewrite/links to sources

--- content.js
+++ content.js
@@ -1,23 +1,13 @@
-const getNameRegex = name => new RegExp(`\\b${name}\\b`, 'g');
-
 const getRandomAddition = item => item.additions[Math.floor(item.additions.length * Math.random())];
-
-const getNewAddition = (item, isEndOfSentence) => {
-  const addition = getRandomAddition(item);
-  return {
-    src: addition.src,
-    text: `${item.name}, ${addition.text}${isEndOfSentence ? '' : ','}`
-  }
-};
 
 // via http://stackoverflow.com/questions/10730309/find-all-text-nodes-in-html-page#answer-10730777
 function findTextNodes(el) {
-  const textNodes = [];
+  const nodes = []
   const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
   while (walker.nextNode()) {
-    textNodes.push(walker.currentNode);
+    nodes.push(walker.currentNode);
   }
-  return textNodes;
+  return nodes;
 }
 
 function generateLink(src) {
@@ -31,43 +21,35 @@ function generateLink(src) {
   return link;
 }
 
-function checkNames() {
-  data.forEach(item => item.isPresent = document.body.innerHTML.includes(`${item.forename} ${item.name}`));
-}
-
 // extend text content with extension sentences
 function extendText() {
+  const items = data
+    .filter(item => document.body.innerHTML.includes(`${item.forename} ${item.name}`))
+    .map(item => Object.assign(item, { regex: new RegExp(`\\b${item.name}\\b`, 'g') }));
+
+  const range = document.createRange();
+
   findTextNodes(document.body).forEach(node => {
     node.textContent = node.textContent.replace(/Björn Höcke/g, 'Bernd Höcke');
-    data.forEach(item => {
-      if (item.isPresent) {
-        const parts = node.textContent.split(getNameRegex(item.name));
-        if (parts.length < 2) return;
-        const srcLinks = [];
-        let currentPos = 0;
-        for (let i = parts.length - 1; i > 0; i--) {
-          const isEndOfSentence = /^\s*[^a-z 0-9]/i.test(parts[i]) || parts[i] === '';
-          const addition = getNewAddition(item, isEndOfSentence);
-          currentPos += parts[i].length;
-          parts.splice(i, 0, addition.text);
-          srcLinks.push({
-            src: addition.src,
-            pos: currentPos + (isEndOfSentence ? 0 : 1),
-          })
-          currentPos += addition.text.length;
-        }
-        node.textContent = parts.join('');
-        const totalLength = node.textContent.length;
-        const range = document.createRange();
-        srcLinks.forEach(link => {
-          range.setStart(node, totalLength - link.pos);
-          range.insertNode(generateLink(link.src));
-        })
+    items.forEach(item => {
+      const indexList = [];
+      let rx;
+      while (rx = item.regex.exec(node.textContent)) {
+        indexList.unshift(rx.index + rx[0].length);
       }
-    });
-  });
+      indexList.forEach(index => {
+        const addition = getRandomAddition(item);
+        const nextChar = node.textContent[index];
+        const isEndOfSentence = !nextChar || nextChar !== ' ';
+        range.setStart(node, index);
+        if (!isEndOfSentence) {
+          range.insertNode(document.createTextNode(','));
+        }
+        range.insertNode(generateLink(addition.src));
+        range.insertNode(document.createTextNode(`, ${addition.text}`));
+      })
+    })
+  })
 }
 
-checkNames();
 extendText();
-
